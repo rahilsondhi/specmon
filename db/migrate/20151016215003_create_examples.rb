f0c104b28a3edb4eb24fff8e93085d2ded8a7857
Rahil Sondhi <rahilsondhi@gmail.com>
2015-10-16 14:50:30 -0700

    Create an Example model

---
+++ db/migrate/20151016215003_create_examples.rb
@@ -0,0 +1,11 @@
+class CreateExamples < ActiveRecord::Migration
+  def change
+    create_table :examples do |t|
+      t.string :file
+      t.string :name
+      t.integer :run_time
+      t.string :vcs_revision
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151016215003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "examples", force: :cascade do |t|
+    t.string   "file"
+    t.string   "name"
+    t.integer  "run_time"
+    t.string   "vcs_revision"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
