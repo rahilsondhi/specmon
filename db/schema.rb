1d1e03510cf51f06937299f142945c8bbde79b1f
Rahil Sondhi <rahilsondhi@gmail.com>
2015-10-16 16:19:41 -0700

    Index vcs_revision on examples

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016215003) do
+ActiveRecord::Schema.define(version: 20151016231839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,5 +24,7 @@ ActiveRecord::Schema.define(version: 20151016215003) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "examples", ["vcs_revision"], name: "index_examples_on_vcs_revision", using: :btree
 
 end
