fc278e0478fb56e009bf14be03bc6a508fb20fd9
Rahil Sondhi <rahilsondhi@gmail.com>
2015-10-29 11:58:00 -0700

    Order examples by slowest first

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,6 +1,6 @@
 class ExamplesController < ApplicationController
   def index
     latest_vcs_revision = Example.last.vcs_revision
-    @examples = Example.where(vcs_revision: latest_vcs_revision).all
+    @examples = Example.where(vcs_revision: latest_vcs_revision).order("run_time desc").all
   end
 end
